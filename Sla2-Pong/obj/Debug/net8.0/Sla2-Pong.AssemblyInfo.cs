587dbb5bed15dc719c46fbab066f268d1d593532
Renas <Renask.dev@gmail.com>
2024-03-11 00:00:32 +0100

    changed the hello world

--- Sla2-Pong/obj/Debug/net8.0/Sla2-Pong.AssemblyInfo.cs
+++ Sla2-Pong/obj/Debug/net8.0/Sla2-Pong.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sla2-Pong")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0604f240c0a1b71539cf6301482e16dda89e79d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0814f60c3952f3b7e92aa229369f40d772f5eb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sla2-Pong")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sla2-Pong")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
