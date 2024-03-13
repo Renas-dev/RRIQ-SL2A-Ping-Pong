ad9ab0f6e871c066b159cebf2f42243d46433e06
Renas <Renask.dev@gmail.com>
2024-03-13 21:47:57 +0100

    Game frame has been added using unicode

--- Sla2-Pong/Program.cs
+++ Sla2-Pong/Program.cs
@@ -1,10 +1,63 @@
-﻿namespace Sla2_Pong
+﻿using System;
+
+namespace Sla2Pong
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, pong");
+            //We set the width and height of the console window.
+            int width = 90; 
+            int height = 30;
+            // Set console encoding to UTF-8, this allows our unicode characters to be displayed correctly.
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            // Set console window size to the width and height of the box + 1, this ensures that the entire box is visible.
+            Console.SetWindowSize(width + 1, height + 1); 
+
+            // We call the Frame method from the GameFrame class and pass the width and height as arguments.
+            GameFrame.Frame(width, height);
+
+            Console.ReadLine(); // Pause to see the box so we can see the render of the box
+        }
+    }
+
+    public static class GameFrame
+    {
+        public static void Frame(int width, int height)
+        {
+            // We draw the top side of the box by using a for loop to draw the horizontal line.
+            Console.SetCursorPosition(0, 0);
+            for (int i = 0; i < width; i++)
+            {
+                Console.Write("\u2501"); 
+            }
+
+            //We draw the bottom side of the box by using a for loop to draw the horizontal line.
+            Console.SetCursorPosition(0, height);
+            for (int i = 0; i < width; i++)
+            {
+                Console.Write("\u2501"); 
+            }
+
+            // For the vertical lines we do the same. We use SetCursorPosition 0 for the left side and, width for the right side.
+            // This ensures that the vertical lines are drawn at the correct position.
+            for (int i = 1; i < height; i++)
+            {
+                Console.SetCursorPosition(0, i);
+                Console.Write("\u2503"); 
+                Console.SetCursorPosition(width, i);
+                Console.Write("\u2503"); 
+            }
+
+            // We use the same practice for the corners.
+            Console.SetCursorPosition(0, 0);
+            Console.Write("\u250F"); 
+            Console.SetCursorPosition(width, 0);
+            Console.Write("\u2513"); 
+            Console.SetCursorPosition(0, height);
+            Console.Write("\u2517"); 
+            Console.SetCursorPosition(width, height);
+            Console.Write("\u251B"); 
         }
     }
 }
--- Sla2-Pong/obj/Debug/net8.0/Sla2-Pong.AssemblyInfo.cs
+++ Sla2-Pong/obj/Debug/net8.0/Sla2-Pong.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sla2-Pong")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0814f60c3952f3b7e92aa229369f40d772f5eb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f991de3155bd6f01bf63a9c8818060640e6082b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sla2-Pong")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sla2-Pong")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
